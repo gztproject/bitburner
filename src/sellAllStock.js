3e84a76b429c774ff31f9f081db943002fde67a3
GZTproject <gasper.doljak@gmail.com>
2022-02-10 09:33:12 +0100

    Fixed stock API

--- src/sellAllStock.js
+++ src/sellAllStock.js
@@ -11,7 +11,7 @@ function localeHHMMSS(ms = 0) {
 
 function sellShorts(ns, stockSymbol) {
   const stockInfo = getStockInfo(ns, stockSymbol)
-  const shortSellValue = ns.sellShort(stockSymbol, stockInfo.sharesShort)
+  const shortSellValue = ns.stock.sellShort(stockSymbol, stockInfo.sharesShort)
 
   if (shortSellValue) {
     ns.print(
@@ -25,7 +25,7 @@ function sellShorts(ns, stockSymbol) {
 
 function sellLongs(ns, stockSymbol) {
   const stockInfo = getStockInfo(ns, stockSymbol)
-  const longSellValue = ns.sellStock(stockSymbol, stockInfo.sharesLong)
+  const longSellValue = ns.stock.sell(stockSymbol, stockInfo.sharesLong)
 
   if (longSellValue) {
     ns.print(
@@ -38,10 +38,10 @@ function sellLongs(ns, stockSymbol) {
 }
 
 function getStockInfo(ns, stockSymbol) {
-  const [sharesLong, avgPriceLong, sharesShort, avgPriceShort] = ns.getStockPosition(stockSymbol)
+  const [sharesLong, avgPriceLong, sharesShort, avgPriceShort] = ns.stock.getPosition(stockSymbol)
 
-  const stockAskPrice = ns.getStockAskPrice(stockSymbol)
-  const stockBidPrice = ns.getStockBidPrice(stockSymbol)
+  const stockAskPrice = ns.stock.getAskPrice(stockSymbol)
+  const stockBidPrice = ns.stock.getBidPrice(stockSymbol)
 
   return {
     stockSymbol,
@@ -57,7 +57,7 @@ function getStockInfo(ns, stockSymbol) {
 export async function main(ns) {
   ns.disableLog('ALL')
 
-  stockSymbols = ns.getStockSymbols()
+  stockSymbols = ns.stock.getSymbols()
   stockSymbols.forEach((stockSymbol) => {
     sellLongs(ns, stockSymbol)
     sellShorts(ns, stockSymbol)
